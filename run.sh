b45d69a0bfdb722b59085e0e28e90c9890a73388
Markus Klotzbuecher <markus.klotzbuecher@mech.kuleuven.be>
2011-09-21 11:26:44 +0200

    don't import rtt

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@ RFSM=`rospack find rfsm`
 OCL=`rospack find ocl`
 export LUA_PATH=";;;$LUA_PATH;$RFSM/?.lua;$OCL/lua/modules/?.lua"
 
-#rosrun ocl deployer-gnulinux -s deploy.ops
-rosrun ocl rttlua-gnulinux -i deploy.lua
+rosrun ocl deployer-gnulinux -s deploy.ops
+#rosrun ocl rttlua-gnulinux -i deploy.lua
